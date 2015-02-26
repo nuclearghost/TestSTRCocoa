b23b330024fe0b1abf6a2073acbea65d9cec5e99
Mark Meyer <akisaka@gmail.com>
2015-02-26 10:26:05 -0800

    Initial Commit

---
+++ TestCocoa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCocoa
+//
+//  Created by Mark Meyer on 2/26/15.
+//  Copyright (c) 2015 Sharethrough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCocoa/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestCocoa
+//
+//  Created by Mark Meyer on 2/26/15.
+//  Copyright (c) 2015 Sharethrough. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
